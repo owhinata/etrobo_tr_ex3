671a2cbb2b1f29459f2c4a1c3cc86ceff581250c
Shintaro.Tomon <tomon@emtechs.co.jp>
2023-09-14 20:36:01 +0900

    feat: #73 Fix arm control

--- walker/TreasureWalker.cpp
+++ walker/TreasureWalker.cpp
@@ -30,6 +30,7 @@ void TreasureWalker::reset() {
     mArmMotor.setPWM(-30);
     while (i < 10) {
         angle = mArmMotor.getCount();
+	// printf("angle=%lf\n", angle);
         if (angle == prevAngle) {
             i++;
         } else {
@@ -39,42 +40,47 @@ void TreasureWalker::reset() {
     }
     mArmMotor.stop();
     mArmMotor.reset();
+    // printf("--------------reset over------------");
 }
 
 bool TreasureWalker::execute() {
     double angle;
     if (state == 0) {
+	// printf("state 0\n");
         angle = (float)mArmMotor.getCount();
-        mArmMotor.setPWM(30);
-        // printf("angle=%lf i=%d pwm=%d\n", angle, i, mPwm);
-        if (angle - 2 < mPrevAngle && mPrevAngle < angle + 2) {
+        mArmMotor.setPWM(-30);
+        // printf("angle=%lf mCount=%d pwm=-30\n", angle, mCount);
+        if (angle - 1 < mPrevAngle && mPrevAngle < angle + 1) {
             mCount++;
         } else {
             mCount = 0;
         }
-        if (mCount > 10) {
+        if (mCount > 20) {
             mArmMotor.stop();
             mCount = 0;
             ++state;
         }
         mPrevAngle = angle;
     } else if (state == 1) {
+	// printf("state 1\n");
         mHsv = mLineMonitor->getHsvColor();
+	mCount++;
         // printf("h=%lf s=%lf v=%lf\n", mHsv.h, mHsv.s, mHsv.v);
         if (mCount > 10) {
             mCount = 0;
             ++state;
         }
     } else if (state == 2) {
+	// printf("state 2\n");
         angle = (float)mArmMotor.getCount();
-        mArmMotor.setPWM(-30);
-        // printf("angle=%lf i=%d pwm=%d\n", angle, i, mPwm);
+        mArmMotor.setPWM(30);
+        // printf("angle=%lf mCount=%d pwm=30\n", angle, mCount);
         if (angle - 2 < mPrevAngle && mPrevAngle < angle + 2) {
             mCount++;
         } else {
             mCount = 0;
         }
-        if (mCount > 10) {
+        if (mCount > 20) {
             mArmMotor.stop();
             mCount = 0;
             ++state;
